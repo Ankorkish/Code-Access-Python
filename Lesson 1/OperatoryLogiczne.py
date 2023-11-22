cd76337ee301f060bc9e2d7a1309562c90e74f44
Ankorkish <58787660+Ankorkish@users.noreply.github.com>
2023-11-22 16:58:43 +0100

    Operatory logiczne

---
+++ Lesson 1/OperatoryLogiczne.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+c = 20
+
+# Operator and zwraca True, jeżeli oba warunki są spełnione
+print(a > b and b < c)  # Wynik: True
+
+# Operator or zwraca True, jeżeli przynajmniej jeden warunek jest spełniony
+print(a < b or b < c)  # Wynik: True
+
+# Operator not zwraca przeciwną wartość danego wyrażenia
+print(not(a > b and b < c))  # Wynik: False
